835b763601d7b055087e8d8a10f69d862f1523e2
Helghardt Avenant <helghardt@gmail.com>
2017-08-25 23:54:33 -0700

    Clean up ico-client for demo.

--- src/app/pages/receive/receive.module.js
+++ src/app/pages/receive/receive.module.js
@@ -4,18 +4,18 @@
   angular.module('BlurAdmin.pages.receive', [])
       .config(routeConfig);
 
-  /** @ngInject */
-  function routeConfig($stateProvider) {
-    $stateProvider
-        .state('receive', {
-          url: '/receive',
-          templateUrl: 'app/pages/receive/receive.html',
-          controller: 'ReceiveCtrl',
-          title: 'Receive',
-          sidebarMeta: {
-            order: 0
-          }
-        });
-  }
+  ///** @ngInject */
+  //function routeConfig($stateProvider) {
+  //  $stateProvider
+  //      .state('receive', {
+  //        url: '/receive',
+  //        templateUrl: 'app/pages/receive/receive.html',
+  //        controller: 'ReceiveCtrl',
+  //        title: 'Receive',
+  //        sidebarMeta: {
+  //          order: 0
+  //        }
+  //      });
+  //}
 
 })();
--- src/app/pages/send/send.module.js
+++ src/app/pages/send/send.module.js
@@ -4,18 +4,18 @@
   angular.module('BlurAdmin.pages.send', [])
       .config(routeConfig);
 
-  /** @ngInject */
-  function routeConfig($stateProvider) {
-    $stateProvider
-        .state('send', {
-          url: '/send',
-          templateUrl: 'app/pages/send/send.html',
-          controller: 'SendCtrl',
-          title: 'Send',
-          sidebarMeta: {
-            order: 0
-          }
-        });
-  }
+  ///** @ngInject */
+  //function routeConfig($stateProvider) {
+  //  $stateProvider
+  //      .state('send', {
+  //        url: '/send',
+  //        templateUrl: 'app/pages/send/send.html',
+  //        controller: 'SendCtrl',
+  //        title: 'Send',
+  //        sidebarMeta: {
+  //          order: 0
+  //        }
+  //      });
+  //}
 
 })();
